be52e86b462965009fa34d7d5e1914cb914bf57a
Santiago Bernaldo <sochoa@visual-engin.com>
2017-06-27 10:46:59 +0200

    Added Walkthrough ViewControllers and Scene+Common

---
+++ HiringApp/HiringApp/Application/Scenes/Walkhthrough/WakthroughPageOneViewController.swift
@@ -0,0 +1,24 @@
+//
+//  WakthroughPageOne.swift
+//  HiringApp
+//
+//  Created by Santi Bernaldo on 27/06/2017.
+//  Copyright © 2017 Visual Engineering. All rights reserved.
+//
+
+import UIKit
+
+class WakthroughPageOneViewController: UIViewController {
+    
+    //MARK: - View lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+    
+    }
+    
+    //MARK: - Private API
+    private func layout() {
+        
+    }
+}
---
+++ HiringApp/HiringApp/Application/Scenes/Walkhthrough/WalkthroughPageTwoViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WalkthroughPageTwo.swift
+//  HiringApp
+//
+//  Created by Santi Bernaldo on 27/06/2017.
+//  Copyright © 2017 Visual Engineering. All rights reserved.
+//
+
+import Foundation
