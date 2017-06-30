acd433626f8826008ca21fdf4948cbadf297dea6
Santiago Bernaldo <sochoa@visual-engin.com>
2017-06-30 13:07:56 +0200

    Page two adapted to speed velocity variation transition

--- HiringApp/HiringApp/Application/Scenes/Walkhthrough/WalkthroughPageTwoViewController.swift
+++ HiringApp/HiringApp/Application/Scenes/Walkhthrough/WalkthroughPageTwoViewController.swift
@@ -11,6 +11,8 @@ import BWWalkthrough
 
 class WalkthroughPageTwoViewController: BWWalkthroughPageViewController {
     
+    var labelMiddleSubtitle = UILabel()
+    
     enum Constants {
         static let labelsTextColor: UIColor = .white
         
@@ -25,44 +27,10 @@ class WalkthroughPageTwoViewController: BWWalkthroughPageViewController {
     
     private let arrayStringLogos = ["apple-logo","android-logo","windowsp-logo","js-logo"]
     
-    //This stackView will contain all stackView childs
-    private let stackViewContainer: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        return stackView
-    }()
-    
-    private let stackViewLogos: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.distribution = .fillEqually
-        return stackView
-    }()
-    
-    private let stackViewLabelsTop: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .fillProportionally
-        
-        return stackView
-    }()
-    
-    private let stackViewLabelsMedium: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .fill
-        stackView.distribution = .fillProportionally
-        
-        return stackView
-    }()
-    
     //MARK: - View lifecycle
     override func viewDidLoad() {
-        configureStackViewsLabels()
-        configureStackViewIcons()
+        configureLabels()
+        configureIcons()
         layout()
         
         super.viewDidLoad()
@@ -74,38 +42,63 @@ class WalkthroughPageTwoViewController: BWWalkthroughPageViewController {
         
         edgesForExtendedLayout = []
         
-        view.addSubviewWithAutolayout(stackViewContainer)
-      
-        stackViewContainer.heightAnchor.constraint(equalToConstant: self.view.frame.size.height*Constants.heightMultiplierStackViewContainer).isActive = true
-        stackViewContainer.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: Constants.lateralSpacingStackViewContainer).isActive = true
-        stackViewContainer.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -Constants.lateralSpacingStackViewContainer).isActive = true
-        stackViewContainer.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
-
-        stackViewLabelsTop.heightAnchor.constraint(equalToConstant: self.view.frame.size.height*Constants.heightMultiplierStackViewContainer/3).isActive = true
-        stackViewLabelsMedium.heightAnchor.constraint(equalToConstant: self.view.frame.size.height*Constants.heightMultiplierStackViewContainer/3).isActive = true
-        stackViewLogos.heightAnchor.constraint(equalToConstant: self.view.frame.size.height*Constants.heightMultiplierStackViewContainer/3).isActive = true
-
-        stackViewContainer.addArrangedSubview(stackViewLabelsTop)
-        stackViewContainer.addArrangedSubview(stackViewLabelsMedium)
-        stackViewContainer.addArrangedSubview(stackViewLogos)
     }
     
-    private func configureStackViewsLabels() {
+    private func configureLabels() {
         let labelTopTitle = self.getLabelTitleWithText("¿Quiénes somos?")
+        view.addSubviewWithAutolayout(labelTopTitle)
+
+        labelTopTitle.topAnchor.constraint(equalTo: view.topAnchor, constant:self.view.frame.height * CGFloat(0.2)).isActive = true
+        labelTopTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width * CGFloat(0.1)).isActive = true
+        labelTopTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width * CGFloat(0.1))).isActive = true
+
         let labelTopSubtitle = self.getLabelSubtitleWithText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+        view.addSubviewWithAutolayout(labelTopSubtitle)
+
+        labelTopSubtitle.topAnchor.constraint(equalTo: labelTopTitle.bottomAnchor, constant: 20).isActive = true
+        labelTopSubtitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width * CGFloat(0.1)).isActive = true
+        labelTopSubtitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width * CGFloat(0.1))).isActive = true
+
         let labelMiddleTitle = self.getLabelTitleWithText("¿Qué hacemos?")
-        let labelMiddleSubtitle = self.getLabelSubtitleWithText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+        view.addSubviewWithAutolayout(labelMiddleTitle)
+
+        labelMiddleTitle.topAnchor.constraint(equalTo: labelTopSubtitle.bottomAnchor, constant: 20).isActive = true
+        labelMiddleTitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width * CGFloat(0.1)).isActive = true
+        labelMiddleTitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width * CGFloat(0.1))).isActive = true
         
-        stackViewLabelsTop.addArrangedSubview(labelTopTitle)
-        stackViewLabelsTop.addArrangedSubview(labelTopSubtitle)
-        stackViewLabelsMedium.addArrangedSubview(labelMiddleTitle)
-        stackViewLabelsMedium.addArrangedSubview(labelMiddleSubtitle)
+        labelMiddleSubtitle = self.getLabelSubtitleWithText("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+        view.addSubviewWithAutolayout(labelMiddleSubtitle)
+
+        labelMiddleSubtitle.topAnchor.constraint(equalTo: labelMiddleTitle.bottomAnchor, constant: 20).isActive = true
+        labelMiddleSubtitle.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: view.frame.width * CGFloat(0.1)).isActive = true
+        labelMiddleSubtitle.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -(view.frame.width * CGFloat(0.1))).isActive = true
     }
     
-    private func configureStackViewIcons() {
+    private func configureIcons() {
         let arrayImageLogos = getArrayImagesLogos()
-        for imageView in arrayImageLogos {
-            stackViewLogos.addArrangedSubview(imageView)
+        
+        for (index,imageView) in arrayImageLogos.enumerated() {
+            view.addSubviewWithAutolayout(imageView)
+            
+            var leadingAnchor = NSLayoutXAxisAnchor()
+            var previousImage = UIImageView()
+            if index > 0 {
+                previousImage = arrayImageLogos[index-1]
+            }
+            let previousLeadingAnchor = previousImage.leadingAnchor
+
+            leadingAnchor = (index == 0) ? view.leadingAnchor : previousLeadingAnchor
+            let constant = (index == 0) ? view.frame.width * CGFloat(0.1) : imageView.frame.width + view.frame.width * CGFloat(0.1)
+
+            if index > 0 {
+                imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: constant).isActive = true
+            }
+            if index == 1 {
+                imageView.trailingAnchor.constraint(equalTo: labelMiddleSubtitle.centerXAnchor).isActive = true
+            }
+            imageView.topAnchor.constraint(equalTo: labelMiddleSubtitle.bottomAnchor, constant: 20).isActive = true
+            imageView.widthAnchor.constraint(equalToConstant: self.view.frame.width * 0.17).isActive = true
+            imageView.heightAnchor.constraint(equalToConstant: self.view.frame.width * 0.17).isActive = true
         }
     }
     
