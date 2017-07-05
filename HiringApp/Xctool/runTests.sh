4b1f74e4e32c7042d5ad74a46f7b52c0ec3f2d88
Margareta Kusan <mkusan@naradarobotics.com>
2017-07-05 15:53:22 +0200

    xctool script ready to execute from terminal

---
+++ HiringApp/Xctool/runTests.sh
@@ -0,0 +1,3 @@
+/usr/local/Cellar/xctool/0.3.2/libexec/bin/xctool -workspace ../HiringApp.xcworkspace -scheme HiringApp run-tests -only HiringAppTests -test-sdk iphonesimulator10.3
+
+/usr/local/Cellar/xctool/0.3.2/libexec/bin/xctool -workspace ../HiringApp.xcworkspace -scheme HiringApp run-tests -only HiringAppCoreTests -test-sdk iphonesimulator10.3
