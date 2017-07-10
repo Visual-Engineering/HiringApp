99f1c49a63b5339a91015f00b80cf1e787c6585f
Margareta Kusan <mkusan@naradarobotics.com>
2017-07-10 09:06:59 +0200

    added class AnalyticsManager

---
+++ HiringApp/HiringApp/Application/Reusable components/Managers/AnalyticsManager.swift
@@ -0,0 +1,14 @@
+//
+//  AnalyticsManager.swift
+//  HiringApp
+//
+//  Created by Margareta Kusan on 10/07/2017.
+//  Copyright © 2017 Visual Engineering. All rights reserved.
+//
+
+import Foundation
+import FirebaseAnalytics
+
+class AnalyticsManager {    
+    
+}
