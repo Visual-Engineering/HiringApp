57845b4d7f932d892ddd1d11df679267a58c2883
Santiago Bernaldo <sochoa@visual-engin.com>
2017-07-25 17:31:12 +0200

    Added Keychain wrapper

---
+++ HiringApp/HiringApp/Utils/Keychain.swift
@@ -0,0 +1,19 @@
+//
+//  Keychain.swift
+//  HiringApp
+//
+//  Created by Santi Bernaldo on 25/07/2017.
+//  Copyright © 2017 Visual Engineering. All rights reserved.
+//
+
+import Foundation
+
+struct Keychain {
+    
+    static let shared = Keychain()
+    
+    let uuidIdentifier = String()
+    
+    let token = String()
+    
+}
