fdf108995ae1d44cde0e6dfc9f4bfcc3ccab75e9
Santiago Bernaldo <sochoa@visual-engin.com>
2017-07-26 11:36:26 +0200

    Generating uuid

--- HiringApp/HiringApp/Utils/Keychain.swift
+++ HiringApp/HiringApp/Utils/Keychain.swift
@@ -10,10 +10,50 @@ import Foundation
 
 struct Keychain {
     
+    struct KeychainConfiguration {
+        static let serviceName = "HiringApp"
+        static let accessGroup: String? = nil
+    }
+    
+    struct Constants {
+        static let userName = "userName"
+    }
+    
     static let shared = Keychain()
     
-    let uuidIdentifier = String()
+    let uuidIdentifier: String
     
-    let token = String()
+    let token:  String
     
+    private init() {
+        uuidIdentifier = ""
+        token = ""
+    }
+    
+    func getUUID() -> String? {
+        let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName,
+                                              account: Constants.userName)
+        
+        guard let uuid = try? passwordItem.readPassword() else {
+            return createUUID()
+        }
+        return uuid
+    }
+    
+    func createUUID() -> String? {
+        let generatedUUID = UIDevice.current.identifierForVendor?.uuidString
+        guard let _generatedUUID = generatedUUID else { return nil }
+        
+        let passwordItem = KeychainPasswordItem(service: KeychainConfiguration.serviceName,
+                                                account: Constants.userName,
+                                                accessGroup: KeychainConfiguration.accessGroup)
+        
+        do {
+            try passwordItem.savePassword(_generatedUUID)
+        } catch {
+            fatalError("Error saving password")
+        }
+        
+        return _generatedUUID
+    }
 }
